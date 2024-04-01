72ce5ee54f9a503cb4a3b9beac6ab1d28123cbfc
enguyen111 <neric77@gmail.com>
2024-04-01 19:01:48 -0400

    migrate sample data from React to Node

---
+++ Kanbas/Database/assignments.js
@@ -0,0 +1,32 @@
+export default [
+  { "_id": "A101", "title": "Propulsion Assignment", "course": "RS101", "type": "ASSIGNMENT" },
+  { "_id": "A102", "title": "Combustion Analysis", "course": "RS101","type": "ASSIGNMENT" },
+  { "_id": "A103", "title": "Nozzle Design Project", "course": "RS101", "type": "ASSIGNMENT" },
+
+  { "_id": "A104", "title": "Propulsion Assignment Quiz", "course": "RS101", "type": "QUIZ" },
+  { "_id": "A105", "title": "Combustion Analysis Quiz", "course": "RS101","type": "QUIZ" },
+  { "_id": "A106", "title": "Nozzle Design Quiz", "course": "RS101", "type": "QUIZ" },
+
+  { "_id": "A107", "title": "RS101 Exam 1", "course": "RS101", "type": "EXAM" },
+  { "_id": "A108", "title": "RS101 Exam 2", "course": "RS101","type": "EXAM" },
+
+  { "_id": "A109", "title": "RS101 Project", "course": "RS101","type": "PROJECT" },
+
+
+  { "_id": "A201", "title": "Aerodynamics Quiz", "course": "RS102", "type": "QUIZ" },
+  { "_id": "A202", "title": "Flow Analysis", "course": "RS102", "type": "ASSIGNMENT" },
+  { "_id": "A203", "title": "Heating Analysis", "course": "RS102", "type": "ASSIGNMENT" },
+  { "_id": "A204", "title": "Flow Analysis Project", "course": "RS102", "type": "PROJECT" },
+  { "_id": "A205", "title": "Heating Analysis Project", "course": "RS102", "type": "PROJECT" },
+
+
+  { "_id": "A301", "title": "Structural Design Task", "course": "RS103", "type": "ASSIGNMENT" },
+  { "_id": "A302", "title": "Orbital Calculations", "course": "RS103", "type": "ASSIGNMENT" },
+  { "_id": "A303", "title": "Structural Design Task Quiz 1", "course": "RS103", "type": "QUIZ" },
+  { "_id": "A304", "title": "Structural Design Task Quiz 2", "course": "RS103", "type": "QUIZ" },
+  { "_id": "A305", "title": "Orbital Calculations Quiz 1", "course": "RS103", "type": "QUIZ" },
+  { "_id": "A306", "title": "Orbital Calculations Quiz 2", "course": "RS103", "type": "QUIZ" },
+  { "_id": "A307", "title": "Systems Engineering Final Exam", "course": "RS103", "type": "EXAM" },
+  { "_id": "A308", "title": "Systems Engineering Final Project", "course": "RS103", "type": "PROJECT" }
+
+]
