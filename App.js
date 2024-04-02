2a90ac8569376042559ff4c5f6c8599e406c67d7
enguyen111 <neric77@gmail.com>
2024-04-01 20:59:08 -0400

    Created an endpoint in server to allow clients to pull the course data

--- App.js
+++ App.js
@@ -8,9 +8,13 @@ import Lab5 from "./Lab5.js";
 import Module from "./Module.js";
 import Todo from "./Todo.js";
 import cors from "cors";
+import CourseRoutes from "./Kanbas/courses/route.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+CourseRoutes(app);
+
 Lab5(app);
 Hello(app);
 Calculator(app);
