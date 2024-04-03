493a6fd1e769c963a864c98d7d42f34a2c15ec35
enguyen111 <neric77@gmail.com>
2024-04-03 00:26:13 -0400

    Added new endpoint to add new assignments

--- Kanbas/courses/assignments/route.js
+++ Kanbas/courses/assignments/route.js
@@ -1,5 +1,23 @@
 import db from "../../Database/index.js";
 function AssignmentRoutes(app) {
+
+    app.post("/api/courses/:cid/assignments", (req, res) => {
+        const { cid } = req.params;
+        const { assignment, assignmentType } = req.body;
+
+        const newAssignment = {
+            _id: new Date().getTime().toString(),
+            course: cid,
+            type: assignmentType,
+            ...assignment,
+        };
+
+        db.assignments.push(newAssignment);
+
+        res.send(newAssignment);
+    });
+
+
     app.get("/api/courses/:cid/assignments", (req, res) => {
         const { cid } = req.params;
         const assignments = db.assignments
