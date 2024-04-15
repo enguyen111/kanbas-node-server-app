75a1cb865f4c70a91131b5e55d85c28ad8be211e
enguyen111 <neric77@gmail.com>
2024-04-15 00:53:24 -0400

    reconfigure project to have sessions

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 //const express = require("express");
+import "dotenv/config";
 import express from "express"
 import Hello from "./Hello.js";
 import Calculator from "./Calculator.js";
@@ -13,10 +14,31 @@ import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/courses/assignments/route.js";
 import mongoose from "mongoose";
 import UserRoutes from "./models/Users/routes.js";
+import session from "express-session";
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    //origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL
+}));
 app.use(express.json());
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    //secret: "my secret key",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+
 
 AssignmentRoutes(app);
 CourseRoutes(app);
