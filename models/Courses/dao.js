f9ef75cc698e665b4c6bfca6508ab75587c4ac0d
enguyen111 <neric77@gmail.com>
2024-04-15 20:09:10 -0400

    refactor: use courseId to retrieve course instead of objectid

--- models/Courses/dao.js
+++ models/Courses/dao.js
@@ -6,6 +6,6 @@ export const createCourse = (course) => {
 };
 
 export const findAllCourses = () => model.find();
-export const findCourseById = (courseId) => model.findById(courseId);
+export const findCourseById = (courseId) => model.find({id: courseId}).exec();
 export const updateCourse = (courseId, course) =>  model.updateOne({ _id: courseId }, { $set: course });
 export const deleteCourse = (courseId) => model.deleteOne({ _id: courseId });
