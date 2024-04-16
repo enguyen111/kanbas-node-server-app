40b90273148eef8ff5b45339c89824723df1f00e
enguyen111 <neric77@gmail.com>
2024-04-15 21:10:13 -0400

    Added new field to keep track of courseIds

--- models/Courses/schema.js
+++ models/Courses/schema.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 const courseSchema = new mongoose.Schema({
+        id: { type: String, required: true, unique: true},
         name: { type: String, required: true, unique: true },
         number: { type: String, required: true, unique: true },
         startDate: String,
